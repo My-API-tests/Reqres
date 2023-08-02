5ed167b43daf0d6abe58cb5cacf1334bbc08a9a9
aksi1984 <pablo_xmen@op.pl>
2023-08-02 09:05:03 +0200

    SuiteTags class implementation

---
+++ src/main/java/org/data/SuiteTags.java
@@ -0,0 +1,9 @@
+package org.data;
+
+public final class SuiteTags {
+
+    public static final String STATUS_CODE = "Status code";
+    public static final String PAYLOAD = "Payload";
+
+    private SuiteTags() { }
+}
