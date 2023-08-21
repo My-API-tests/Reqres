146b1ab9a49fc0ae474c91dcbda3e104345a5194
aksi1984 <pablo_xmen@op.pl>
2023-08-21 23:47:06 +0200

    Added the VALIDATE_HEADERS tag

--- src/main/java/org/data/SuiteTags.java
+++ src/main/java/org/data/SuiteTags.java
@@ -4,6 +4,7 @@ public final class SuiteTags {
 
     public static final String STATUS_CODE = "Status code";
     public static final String PAYLOAD = "Payload";
+    public static final String VALIDATE_HEADERS = "Validate headers";
 
     private SuiteTags() { }
 }
