c88356ce80490715cdfc500cfc84414c146db0f2
aksi1984 <pablo_xmen@op.pl>
2023-09-01 22:28:40 +0200

    Added tag

--- src/main/java/org/qa/constans/SuiteTags.java
+++ src/main/java/org/qa/constans/SuiteTags.java
@@ -1,10 +1,11 @@
-package org.data;
+package org.qa.constans;
 
 public final class SuiteTags {
 
     public static final String STATUS_CODE = "Status code";
     public static final String PAYLOAD = "Payload";
     public static final String VALIDATE_HEADERS = "Validate headers";
+    public static final String PERFORMANCE_SANITY = "Performance sanity";
 
     private SuiteTags() { }
 }
