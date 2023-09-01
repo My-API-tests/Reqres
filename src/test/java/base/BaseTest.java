91688f43c390c59a24e83dabaad68105bca583c5
aksi1984 <pablo_xmen@op.pl>
2023-09-01 22:18:18 +0200

    Added getResponse methods

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,7 +1,12 @@
 package base;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import static io.restassured.RestAssured.given;
 
 public class BaseTest {
 
@@ -9,5 +14,20 @@ public class BaseTest {
     public static void init() {
 
         RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    protected Response getResponse(Method method, String url) {
+
+        RequestSpecification requestSpecification = given();
+
+        return requestSpecification.contentType(ContentType.JSON).request(method, url);
+    }
+
+    protected<T> Response getResponse(Method method, String url, T body) {
+
+        RequestSpecification requestSpecification = given();
+        requestSpecification.contentType(ContentType.JSON).body(body);
+
+        return requestSpecification.request(method, url);
     }
 }
