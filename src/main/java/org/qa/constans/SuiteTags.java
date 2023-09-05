0d6da64673322dfc0a12b5eb7fbe0eb2d386b249
aksi1984 <pablo_xmen@op.pl>
2023-09-05 23:15:06 +0200

    Renamed tags

--- src/main/java/org/qa/constans/SuiteTags.java
+++ src/main/java/org/qa/constans/SuiteTags.java
@@ -2,8 +2,8 @@ package org.qa.constans;
 
 public final class SuiteTags {
 
-    public static final String STATUS_CODE = "Status code";
-    public static final String PAYLOAD = "Payload";
+    public static final String VALIDATE_STATUS_CODE = "Status code";
+    public static final String VALIDATE_PAYLOAD = "Payload";
     public static final String VALIDATE_HEADERS = "Validate headers";
     public static final String PERFORMANCE_SANITY = "Performance sanity";
 
