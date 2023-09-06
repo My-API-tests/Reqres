b9ab5ba56f6bbc1fb94dd212c03f386c1fd3ba4c
aksi1984 <pablo_xmen@op.pl>
2023-09-06 16:32:36 +0200

    Added system info

--- src/test/java/reportsManager/ExtentReportsManager.java
+++ src/test/java/reportsManager/ExtentReportsManager.java
@@ -11,7 +11,7 @@ public class ExtentReportsManager {
     private static ExtentReports extentReports;
     private static ExtentTest extentTest;
 
-    private static final String filepath = "./reports/";
+    private static final String filepath = "./src/reports/";
 
     public static void init(String filename) {
 
@@ -28,7 +28,13 @@ public class ExtentReportsManager {
 
     public static void setInfo() {
 
-
+        extentReports.setSystemInfo("Available processors (core)", String.valueOf(Runtime.getRuntime().availableProcessors()));
+        extentReports.setSystemInfo("Maximum memory", String.valueOf(Runtime.getRuntime().maxMemory()));
+        extentReports.setSystemInfo("Total memory", String.valueOf(Runtime.getRuntime().totalMemory()));
+        extentReports.setSystemInfo("Free memory", String.valueOf(Runtime.getRuntime().freeMemory()));
+        extentReports.setSystemInfo("Operating system", System.getProperty("os.name") + " " + System.getProperty("os.arch"));
+        extentReports.setSystemInfo("System property", System.getProperty("user.dir"));
+        extentReports.setSystemInfo("Java runtime version", System.getProperty("java.runtime.version"));
     }
 
     public static void setTestPassed(String message) {
