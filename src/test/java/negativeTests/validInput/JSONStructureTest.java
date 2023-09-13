1f32f03fb0728f93b4a45dfe72f4e49adcafff80
aksi1984 <pablo_xmen@op.pl>
2023-09-13 06:48:06 +0200

    Added tests checking the structure of JSON data

--- src/test/java/negativeTests/validInput/JSONStructureTest.java
+++ src/test/java/negativeTests/validInput/JSONStructureTest.java
@@ -4,8 +4,6 @@ import base.BaseTest;
 import io.restassured.http.Method;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
-import org.apache.http.HttpStatus;
-import org.junit.jupiter.api.DisplayName;
 import org.qa.bodies.RegisterBody;
 import org.testng.annotations.Test;
 
@@ -22,44 +20,53 @@ public class JSONStructureTest extends BaseTest {
         response.then()
                 .assertThat()
                 .body(JsonSchemaValidator.matchesJsonSchema(inputStream));
-
-        //System.out.println(response.prettyPrint());
     }
 
     @Test
     public void getSingleUser() {
 
-        check(getResponse(Method.GET, "/api/users/250"), "negative-valid-input-get-single-user.json");
+        check(getResponse(Method.GET, "/api/users/250"), "negative-valid-input-empty-body.json");
     }
 
     @Test
     public void getSingleResource() {
 
-        check(getResponse(Method.GET, "/api/unknown/2000"),"negative-valid-input-get-single-resource.json");
+        check(getResponse(Method.GET, "/api/unknown/2000"),"negative-valid-input-empty-body.json");
     }
 
     @Test
     public void registerNotDefinedUser() {
 
-        check(getResponse(Method.POST, "/api/register", new RegisterBody("eve.holt@req.in", "pistol")),"negative-valid-input-register-not-defined.json");
+        check(getResponse(Method.POST, "/api/register", new RegisterBody("eve.holt@req.in", "pistol")),"negative-valid-input-register-login.json");
     }
 
     @Test
     public void registerMissingEmailOrUsername() {
 
-        check(getResponse(Method.POST, "/api/register", new RegisterBody("", "pistol")), "negative-valid-input-register-not-defined.json");
+        check(getResponse(Method.POST, "/api/register", new RegisterBody("", "pistol")), "negative-valid-input-register-login.json");
     }
 
     @Test
     public void registerMissingPassword() {
 
-        check(getResponse(Method.POST, "/api/register", new RegisterBody("eve.holt@reqres.in", "")), "negative-valid-input-register-not-defined.json");
+        check(getResponse(Method.POST, "/api/register", new RegisterBody("eve.holt@reqres.in", "")), "negative-valid-input-register-login.json");
     }
 
     @Test
-    @DisplayName("Should return status code 200")
     public void loginIncorrectUsername() {
 
-        check(getResponse(Method.POST, "/api/login", new RegisterBody("eve.holt@req.in", "pistol")), "negative-valid-input-register-not-defined.json");
+        check(getResponse(Method.POST, "/api/login", new RegisterBody("eve.holt@req.in", "pistol")), "negative-valid-input-register-login.json");
+    }
+
+    @Test
+    public void loginMissingEmailOrUsername() {
+
+        check(getResponse(Method.POST, "/api/login", new RegisterBody("", "pistol")), "negative-valid-input-register-login.json");
+    }
+
+    @Test
+    public void loginMissingPassword() {
+
+        check(getResponse(Method.POST, "/api/login", new RegisterBody("eve.holt@reqres.in", "")), "negative-valid-input-register-login.json");
     }
 }
