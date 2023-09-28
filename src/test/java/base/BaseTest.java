fe7ecd2c5038ed1445a860dbbd16dc3cc181c335
aksi1984 <pablo_xmen@op.pl>
2023-09-28 16:42:38 +0200

    Added method that creates environment data for the report

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,19 +1,41 @@
 package base;
 
+import com.google.common.collect.ImmutableMap;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
 import static io.restassured.RestAssured.given;
+import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 
 public class BaseTest {
 
-    @BeforeClass
-    public static void init() {
+    @BeforeSuite
+    public void init() {
 
         RestAssured.baseURI = "https://reqres.in/";
+        setAllureEnvironment();
+    }
+
+    protected void setAllureEnvironment() {
+
+        allureEnvironmentWriter(
+
+                ImmutableMap.<String, String>builder()
+                        .put("Available processors (core)", String.valueOf(Runtime.getRuntime().availableProcessors()))
+                        .put("Maximum memory", String.valueOf(Runtime.getRuntime().maxMemory()))
+                        .put("Total memory", String.valueOf(Runtime.getRuntime().totalMemory()))
+                        .put("Free memory", String.valueOf(Runtime.getRuntime().freeMemory()))
+                        .put("Available processors", String.valueOf(Runtime.getRuntime().availableProcessors()))
+                        .put("System property", System.getProperty("user.dir"))
+                        .put("Operating system", System.getProperty("os.name") + " " + System.getProperty("os.arch"))
+                        .put("Java runtime version", System.getProperty("java.runtime.version"))
+                        .put("URL", "https://requres.in/")
+                        .build()
+        );
     }
 
     protected Response getResponse(Method method, String url) {
