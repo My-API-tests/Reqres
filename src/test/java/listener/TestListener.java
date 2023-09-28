02219afeaddf9ba4f13e7cb7bc8ebec8a49f9926
aksi1984 <pablo_xmen@op.pl>
2023-09-28 16:52:37 +0200

    Methods responsible for providing data to Alure reports have been added

--- src/test/java/listener/TestListener.java
+++ src/test/java/listener/TestListener.java
@@ -4,8 +4,20 @@ import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import reportsManager.ExtentReportsManager;
+import io.qameta.allure.Attachment;
 
 public class TestListener implements ITestListener {
+
+    private static String getTestMethodName(ITestResult iTestResult) {
+
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog(String message) {
+
+        return message;
+    }
 
     @Override
     public void onStart(ITestContext iTestContext) {
@@ -24,17 +36,23 @@ public class TestListener implements ITestListener {
     public void onTestSuccess(ITestResult iTestResult) {
 
         ExtentReportsManager.setTestPassed(iTestResult.getMethod().getMethodName());
+
+        saveTextLog(getTestMethodName(iTestResult) + "- passed");
     }
 
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
 
         ExtentReportsManager.setTestSkipped(iTestResult.getMethod().getMethodName());
+
+        saveTextLog(getTestMethodName(iTestResult) + "- skipped");
     }
 
     @Override
     public void onTestFailure(ITestResult iTestResult) {
 
         ExtentReportsManager.setTestFailed(iTestResult.getMethod().getMethodName() + " " + iTestResult.getThrowable().getMessage());
+
+        saveTextLog(getTestMethodName(iTestResult) + "- failed");
     }
 }
