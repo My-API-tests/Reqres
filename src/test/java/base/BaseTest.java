727e579ff31b250575788602a383bfacac107f99
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-18 21:55:13 +0100

    Added the method

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,23 +3,43 @@ package base;
 import com.google.common.collect.ImmutableMap;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import io.restassured.http.Method;
+import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.json.JSONObject;
+import org.json.simple.parser.ParseException;
+import org.qa.modelsbuilder.ModelsBuilder;
+import org.qa.testdataloader.TestdataLoader;
+import org.qa.utils.JSONSchemas;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
 import static io.restassured.RestAssured.*;
 
 public class BaseTest {
 
+    @Parameters({"fileName"})
     @BeforeSuite
-    public void init() {
+    public void init(@Optional("noFile") String fileName) throws IOException, ParseException {
 
         RestAssured.baseURI = "https://reqres.in/";
         setAllureEnvironment();
+        TestdataLoader.loadJsonSchemas();
+
+        if (!fileName.equals("noFile")) {
+            TestdataLoader.loadBodies(fileName);
+        }
     }
 
     protected void setAllureEnvironment() {
@@ -38,6 +58,14 @@ public class BaseTest {
                         .put("URL", "https://requres.in/")
                         .build()
         );
+    }
+
+    protected void checkHeaders(Response response) {
+
+        List<String> expected = new ArrayList<>(List.of(ModelsBuilder.getHeaders()));
+        List<String> given = response.headers().asList().stream().map(Header::getName).toList();
+
+        Assert.assertTrue(given.containsAll(expected), "No complete headers");
     }
 
     protected Response getResponse(Method method, String url) {
