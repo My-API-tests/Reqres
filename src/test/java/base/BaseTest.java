800c2c04d29c0e7e637dcedb8bb52c52f465b53b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-19 11:06:09 +0100

    Removed the unnecessary method

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,30 +2,21 @@ package base;
 
 import com.google.common.collect.ImmutableMap;
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.http.Header;
-import io.restassured.http.Method;
-import io.restassured.module.jsv.JsonSchemaValidator;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.apache.http.HttpStatus;
-import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
-import org.qa.modelsbuilder.ModelsBuilder;
+import org.qa.jsondatatransformer.JSONDataTransformer;
 import org.qa.testdataloader.TestdataLoader;
-import org.qa.utils.JSONSchemas;
 import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.github.automatedowl.tools.AllureEnvironmentWriter.allureEnvironmentWriter;
-import static io.restassured.RestAssured.*;
 
 public class BaseTest {
 
@@ -62,32 +53,9 @@ public class BaseTest {
 
     protected void checkHeaders(Response response) {
 
-        List<String> expected = new ArrayList<>(List.of(ModelsBuilder.getHeaders()));
+        List<String> expected = new ArrayList<>(List.of(JSONDataTransformer.getHeaders()));
         List<String> given = response.headers().asList().stream().map(Header::getName).toList();
 
         Assert.assertTrue(given.containsAll(expected), "No complete headers");
-    }
-
-    protected Response getResponse(Method method, String url) {
-
-        RequestSpecification requestSpecification = given();
-
-        return requestSpecification.contentType(ContentType.JSON).request(method, url);
-    }
-
-    protected<T> Response getResponse(Method method, String url, T body) {
-
-        RequestSpecification requestSpecification = given();
-        requestSpecification.contentType(ContentType.JSON).body(body);
-
-        return requestSpecification.request(method, url);
-    }
-
-    protected Response getResponse(Method method, String url, InputStream inputStream) {
-
-        RequestSpecification requestSpecification = given();
-        requestSpecification.contentType(ContentType.JSON).body(inputStream);
-
-        return requestSpecification.request(method, url);
     }
 }
