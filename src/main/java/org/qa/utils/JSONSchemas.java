154a5c12dbb8723488d945188b93e5c645401ed9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-19 11:13:42 +0100

    Added fields

--- src/main/java/org/qa/utils/JSONSchemas.java
+++ src/main/java/org/qa/utils/JSONSchemas.java
@@ -3,9 +3,13 @@ package org.qa.utils;
 public class JSONSchemas {
 
     public static final String EMPTY_BODY = "emptyBody";
-    public static final String  CREATE_USER = "createUser";
+    public static final String CREATE_USER = "createUser";
+    public static final String UPDATE_USER = "updateUser";
     public static final String SINGLE_USER = "singleUser";
     public static final String SINGLE_RESOURCE = "singleResource";
+    public static final String REGISTER = "register";
+    public static final String ERROR_RESPONSE = "errorResponse";
+    public static final String LOGIN = "login";
     public static final String LIST_USERS = "listUsers";
     public static final String DELAYED_RESPONSE = "delayedResponse";
 }
