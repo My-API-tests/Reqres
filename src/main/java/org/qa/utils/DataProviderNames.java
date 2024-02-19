6908d506bcc742db3808a6d09392e5f22ebf36db
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-19 11:14:44 +0100

    Added the field

--- src/main/java/org/qa/utils/DataProviderNames.java
+++ src/main/java/org/qa/utils/DataProviderNames.java
@@ -3,10 +3,11 @@ package org.qa.utils;
 public class DataProviderNames {
 
     public static final String CORRECT = "correct";
-    public static final String WITHOUT_NAME = "withoutName";
-    public static final String WITHOUT_JOB = "withoutJob";
+    public static final String MISSING_NAME = "withoutName";
+    public static final String MISSING_JOB = "withoutJob";
     public static final String INCORRECT_EMAIL = "incorrectEmail";
     public static final String INCORRECT_PASSWORD = "incorrectPassword";
-    public static final String WITHOUT_USERNAME = "withoutUsername";
-    public static final String WITHOUT_PASSWORD = "withoutPassword";
+    public static final String MISSING_EMAIL = "withoutEmail";
+    public static final String MISSING_PASSWORD = "withoutPassword";
+    public static final String USER_NOT_DEFINED = "userNotDefined";
 }
