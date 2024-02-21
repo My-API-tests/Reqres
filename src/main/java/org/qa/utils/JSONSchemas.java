3f37bffa5c45a6e27fb3001143aa1a0479be293a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-21 08:13:40 +0100

    Added fields

--- src/main/java/org/qa/utils/JSONSchemas.java
+++ src/main/java/org/qa/utils/JSONSchemas.java
@@ -4,6 +4,8 @@ public class JSONSchemas {
 
     public static final String EMPTY_BODY = "emptyBody";
     public static final String CREATE_USER = "createUser";
+    public static final String MISSING_NAME = "createUserMissingName";
+    public static final String MISSING_JOB = "createUserMissingJob";
     public static final String UPDATE_USER = "updateUser";
     public static final String SINGLE_USER = "singleUser";
     public static final String SINGLE_RESOURCE = "singleResource";
