f7d985a003542a69ca6c48dc45ff03efb309e02d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-22 08:41:12 +0100

    Added steps

--- src/test/java/SingleResourceTest.java
+++ src/test/java/SingleResourceTest.java
@@ -6,6 +6,8 @@ import org.qa.utils.JSONSchemas;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.isA;
+import static org.hamcrest.Matchers.matchesPattern;
 
 @Epic("E2E")
 @Feature("Singe <Resource>")
@@ -17,6 +19,30 @@ public class SingleResourceTest extends BaseTest {
                 .get("/api/unknown/" + id);
     }
 
+    @Step("Verify {id, name, year, color, pantone_value} data types in the {data} JSON object")
+    private void verifyDataTypesInDataJSONObject(Response response) {
+
+        response
+                .then()
+                .assertThat()
+                .body("data.id", isA(Integer.class))
+                .body("data.name", isA(String.class))
+                .body("data.year", isA(Integer.class))
+                .body("data.color", isA(String.class))
+                .body("data.pantone_value", isA(String.class));
+    }
+
+    @Step("Verify the {color} format")
+    private void verifyColorPropertyValueInResponseWithRequest(Response response) {
+
+        String regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+
+        response
+                .then()
+                .assertThat()
+                .body("data.color", matchesPattern(regex));
+    }
+
     @Severity(SeverityLevel.NORMAL)
     @Description("Verify that the single resource can be retrieved using the correct ID")
     @Story("As an user, I want to be able to retrieve the single resource using the correct ID")
@@ -26,6 +52,9 @@ public class SingleResourceTest extends BaseTest {
         Response response = check("2");
         verifyStatusCode(response, HttpStatus.SC_OK);
         verifyJSONSchema(response, JSONSchemas.SINGLE_RESOURCE);
+        verifyDataTypesInDataJSONObject(response);
+        verifyColorPropertyValueInResponseWithRequest(response);
+
     }
 
     @Description("Verify that an error message appears when an incorrect ID is provided")
