a7a73ac5571a859a6fb45b7b61ef2e06a6f240b6
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-23 22:27:43 +0100

    Created

---
+++ src/main/java/org/qa/support/Patterns.java
@@ -0,0 +1,10 @@
+package org.qa.support;
+
+public class Patterns {
+
+    public static final String EMAIL_FORMAT = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    public static final String COLOR_FORMAT = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
+    public static final String PANTONE_FORMAT = "^([1-9]{2}-2[0-9]{3})$";
+    public static final String AVATAR_FORMAT = "^(https:\\/\\/reqres\\.in\\/img\\/faces\\/.+\\.jpg)$";
+    public static final String DATE_TIME_FORMAT = "^(2[0-9]{3}-[0-1][0-9]-[0-3][0-9]T[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\\.\\d{3}Z)$";
+}
