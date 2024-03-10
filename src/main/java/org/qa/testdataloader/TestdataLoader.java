495ef135e9af64de20f64c7f3ed6841fb3cbbf0b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-10 14:13:55 +0100

    Added the field and the method

--- src/main/java/org/qa/testdataloader/TestdataLoader.java
+++ src/main/java/org/qa/testdataloader/TestdataLoader.java
@@ -8,24 +8,36 @@ import org.json.simple.parser.ParseException;
 
 public class TestdataLoader {
 
-    private static final String schemasPath = "./src/test/resources/testdata/schemas/schemas.json";
-    private static final String bodiesPath = "./src/test/resources/testdata/bodies/";
+    private static final String SCHEMAS_PATH = "./src/test/resources/testdata/schemas/schemas.json";
+    private static final String HEADERS_PATH = "./src/test/resources/testdata/headers/headers.json";
+    private static final String BODIES_PATH = "./src/test/resources/testdata/bodies/";
+
     @Getter
     private static String bodySource;
+    @Getter
+    private static String headersSource;
     @Getter
     private static String schemasSource;
 
     public static void loadJsonSchemas() throws IOException, ParseException {
 
-        Reader fileReader = new FileReader(schemasPath);
+        Reader fileReader = new FileReader(SCHEMAS_PATH);
         JSONParser jsonParser = new JSONParser();
         Object object = jsonParser.parse(fileReader);
         schemasSource = object.toString();
     }
 
+    public static void loadHeaders() throws IOException, ParseException {
+
+        Reader fileReader = new FileReader(HEADERS_PATH);
+        JSONParser jsonParser = new JSONParser();
+        Object object = jsonParser.parse(fileReader);
+        headersSource = object.toString();
+    }
+
     public static void loadBodies(String fileName) throws IOException, ParseException {
 
-        Reader fileReader = new FileReader(bodiesPath + fileName);
+        Reader fileReader = new FileReader(BODIES_PATH + fileName);
         JSONParser jsonParser = new JSONParser();
         Object object = jsonParser.parse(fileReader);
         bodySource = object.toString();
