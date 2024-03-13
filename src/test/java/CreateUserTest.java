e6cd57c763e24c1b341df8487c019f03c5d0b95b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-13 18:39:26 +0100

    Added methods

--- src/test/java/CreateUserTest.java
+++ src/test/java/CreateUserTest.java
@@ -1,5 +1,7 @@
 import base.BaseTest;
 import io.qameta.allure.*;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.http.HttpStatus;
@@ -11,33 +13,54 @@ import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isA;
 
 @Epic("E2E")
 @Feature("Create")
 public class CreateUserTest extends BaseTest {
 
-    private Response set(JSONObject body) {
+    @io.qameta.allure.Step("Perform a POST request to https://reqres.in/api/users")
+    @io.qase.api.annotation.Step("Perform a POST request to https://reqres.in/api/users")
+    private Response sendRequest(String requestBody) {
 
         return given().when()
                 .contentType(ContentType.JSON)
-                .body(body.toString())
+                .body(requestBody)
                 .post("/api/users/");
     }
 
     @Step("Verify {name, job, id, createdAt} data types")
     private void verifyDataTypesInResponse(Response response) {
 
-        response
-                .then()
-                .assertThat()
-                .body("name", isA(String.class))
-                .body("job", isA(String.class))
-                .body("id", isA(String.class))
-                .body("createdAt", isA(String.class));
+        checkDataType(response, "name", String.class);
+        checkDataType(response, "job", String.class);
+        checkDataType(response, "id", String.class);
+        checkDataType(response, "createdAt", String.class);
     }
 
-    @Step("Verify {name, job, id, createdAt} values")
+    @Step("Verify {job, id, createdAt} data type")
+    private void verifyDataTypesWhenMissingName(Response response) {
+
+        checkDataType(response, "job", String.class);
+        checkDataType(response, "id", String.class);
+        checkDataType(response, "createdAt", String.class);
+    }
+
+    @Step("Verify {name, id, createdAt} data type")
+    private void verifyDataTypesWhenMissingJob(Response response) {
+
+        checkDataType(response, "name", String.class);
+        checkDataType(response, "id", String.class);
+        checkDataType(response, "createdAt", String.class);
+    }
+
+    @Step("Verify {id, createdAt} data type")
+    private void verifyDataTypesWhenEmptyRequestBody(Response response) {
+
+        checkDataType(response, "id", String.class);
+        checkDataType(response, "createdAt", String.class);
+    }
+
+    @Step("Verify {name, job} values")
     private void verifyValuesInResponseWithRequest(Response response, JSONObject requestBody) {
 
         response.then()
@@ -64,40 +87,86 @@ public class CreateUserTest extends BaseTest {
                 .body("name", equalTo(requestBody.get("name")));
     }
 
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Verify that a new user can be created using correct data")
-    @Story("As an user, I want to be able to create an account using correct data")
     @Test(dataProvider = DataProviderNames.CORRECT, dataProviderClass = UserDataProviders.class)
+    @QaseId(9)
+    @QaseTitle("Creating a user using correct credentials")
+    @Description("Creating a user using correct credentials")
     public void correct(JSONObject requestBody) {
 
-        Response response = set(requestBody);
+        Response response = sendRequest(requestBody.toString());
         verifyStatusCode(response, HttpStatus.SC_CREATED);
         verifyJSONSchema(response, JSONSchemas.CREATE_USER);
         verifyDataTypesInResponse(response);
         verifyValuesInResponseWithRequest(response, requestBody);
     }
 
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Verify that a new user cannot be created when the \"name\" parameter is missing")
-    @Story("As an user, I want to be able to create an account even if I do not provide a name")
     @Test(dataProvider = DataProviderNames.MISSING_NAME, dataProviderClass = UserDataProviders.class)
+    @QaseId(10)
+    @QaseTitle("Creating a user missing the {name}")
+    @Description("Creating a user missing the {name}")
     public void missingName(JSONObject requestBody) {
 
-        Response response = set(requestBody);
+        Response response = sendRequest(requestBody.toString());
         verifyStatusCode(response, HttpStatus.SC_CREATED);
         verifyJSONSchema(response, JSONSchemas.MISSING_NAME);
+        verifyDataTypesWhenMissingName(response);
         verifyJobValueInResponseWithRequest(response, requestBody);
+        verifyHeaders(response);
     }
 
-    @Severity(SeverityLevel.CRITICAL)
-    @Description("Verify that a new user cannot be created when the \"job\" parameter is missing")
-    @Story("As an user, I want to be able to create an account even if I do not provide a job")
     @Test(dataProvider = DataProviderNames.MISSING_JOB, dataProviderClass = UserDataProviders.class)
+    @QaseId(11)
+    @QaseTitle("Creating a user missing the {job}")
+    @Description("Creating a user missing the {job}")
     public void missingJob(JSONObject requestBody) {
 
-        Response response = set(requestBody);
+        Response response = sendRequest(requestBody.toString());
         verifyStatusCode(response, HttpStatus.SC_CREATED);
         verifyJSONSchema(response, JSONSchemas.MISSING_JOB);
+        verifyDataTypesWhenMissingJob(response);
         verifyNameValueInResponseWithRequest(response, requestBody);
+        verifyHeaders(response);
+    }
+
+    @Test
+    @QaseId(12)
+    @QaseTitle("Creating a user with empty request body")
+    @Description("Creating a user with empty request body")
+    public void emptyRequestBody() {
+
+        Response response = sendRequest("{}");
+        verifyDataTypesWhenEmptyRequestBody(response);
+        verifyHeaders(response);
+    }
+
+    @Test(dataProvider = DataProviderNames.INCORRECT_KEYS, dataProviderClass = UserDataProviders.class)
+    @QaseId(13)
+    @QaseTitle("Creating a user with incorrect keys in the request body")
+    @Description("Creating a user with incorrect keys in the request body")
+    public void incorrectKeyInRequestBody(JSONObject requestBody) {
+
+        Response response = sendRequest(requestBody.toString());
+        verifyStatusCode(response, HttpStatus.SC_BAD_REQUEST);
+        verifyBadRequestResponseBody(response.getBody().asString());
+        verifyHeaders(response);
+    }
+
+    @Test
+    @QaseId(14)
+    @QaseTitle("Creating a user with malformed request body")
+    @Description("Creating a user with malformed request body")
+    public void malformedRequestBody() {
+
+        String JSON = """
+                {
+                    "name": "morpheus",
+                    "salary": ,
+                }
+                """;
+
+        Response response = sendRequest(JSON);
+        verifyStatusCode(response, HttpStatus.SC_BAD_REQUEST);
+        verifyBadRequestResponseBody(response.getBody().asString());
+        verifyHeaders(response);
     }
 }
